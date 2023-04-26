fbb7cffa8d1c6ae8b4944e58d01b366b4f76698d
Steven <Briggs.steven369@gmail.com>
2023-04-25 19:10:16 -0700

    Story Generator 5-min mockup

--- src/main/java/studio/rrprojects/srl/Main.java
+++ src/main/java/studio/rrprojects/srl/Main.java
@@ -1,12 +1,14 @@
 package studio.rrprojects.srl;
 
 import studio.rrprojects.srl.creation.priority.PriorityController;
+import studio.rrprojects.srl.story.StoryGeneratorController;
 
 public class Main {
 
     public static void main(String[] args) {
-        PriorityController priorityController = new PriorityController();
+//        PriorityController priorityController = new PriorityController();
 
-        priorityController.getMasterPriorityMap().forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+//        priorityController.getMasterPriorityMap().forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+        StoryGeneratorController SGC = new StoryGeneratorController();
     }
 }
--- src/main/java/studio/rrprojects/srl/story/StoryGeneratorController.java
+++ src/main/java/studio/rrprojects/srl/story/StoryGeneratorController.java
@@ -1,4 +1,39 @@
 package studio.rrprojects.srl.story;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class StoryGeneratorController {
+
+    private static final String[] STORY_PART_CONTACT = {"Phone Call", "Email", "Matrix Message", "In-Person Contact", "Fixer"};
+    private static final String[] STORY_PART_INTRODUCTION = {"Restaurant", "Matrix", "Office Space", "Public Location", "Dead Drop"};
+    private static final String[] STORY_PART_LEADS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+    private static final String[] STORY_PART_PAYOFF = {"Satisfactory", "Exemplary", "Betrayal", "Twist"};
+
+    public StoryGeneratorController() {
+        System.out.println("Generating a story...");
+        List<String> storyLeads = getRandomLeads();
+        String output = String.format("The Player is contacted in the form of a %s, and is directed to a %s meetup. They use the following leads in the run: %s. Finally the payoff ends in a %s ending",
+                getRandomElement(STORY_PART_CONTACT), getRandomElement(STORY_PART_INTRODUCTION), storyLeads, getRandomElement(STORY_PART_PAYOFF));
+        System.out.println(output);
+    }
+
+    public static List<String> getRandomLeads() {
+        Random rand = new Random();
+        int numParts = rand.nextInt(3) + 1; // choose 1-3 parts
+        List<String> storyLeads = new ArrayList<>();
+        while (storyLeads.size() < numParts) {
+            String part = getRandomElement(STORY_PART_LEADS);
+            if (!storyLeads.contains(part)) {
+                storyLeads.add(part);
+            }
+        }
+        return storyLeads;
+    }
+
+    private static String getRandomElement(String[] array) {
+        Random rand = new Random();
+        return array[rand.nextInt(array.length)];
+    }
 }
