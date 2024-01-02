06e972f0786260d119cd04326c12c78873ccd12c
Steven <Briggs.steven369@gmail.com>
2024-01-02 08:52:17 -0800

    Add MagicKeywords

--- src/main/java/studio/rrprojects/srl/creation/character/modules/MagicModule.java
+++ src/main/java/studio/rrprojects/srl/creation/character/modules/MagicModule.java
@@ -1,11 +1,24 @@
 package studio.rrprojects.srl.creation.character.modules;
 
 import studio.rrprojects.srl.creation.character.CharacterModule;
+import studio.rrprojects.srl.keywords.MagicKeywords;
 
 public class MagicModule extends CharacterModule {
     private String[] creationAvailableMagicOptions;
+    private Boolean isAwakened = true;
 
     public void processPriorityOption(Object magicOptions) {
         creationAvailableMagicOptions = (String[]) magicOptions;
+
+
+        for (String option : creationAvailableMagicOptions) {
+            if (option.equalsIgnoreCase(MagicKeywords.MUNDANE)) {
+                isAwakened = false;
+            }
+        }
+    }
+
+    public Boolean getAwakened() {
+        return isAwakened;
     }
 }
---
+++ src/main/java/studio/rrprojects/srl/keywords/MagicKeywords.java
@@ -0,0 +1,6 @@
+package studio.rrprojects.srl.keywords;
+
+public class MagicKeywords {
+    public static final String MUNDANE = "Mundane";
+
+}
